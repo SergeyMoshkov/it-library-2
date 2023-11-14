6068da362e41a4469bebdb2bed43d19daf456968
MoshkovSergey <moshkov_s@vk.com>
2023-11-14 16:43:43 +0300

    simple app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,23 @@
-import Header from './components/header/Header'
-import MyResponsiveGeoMapCanvas from './components/content/Content'
+import { ColorModeContext, useMode } from './theme'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { useTheme } from '@mui/material/styles'
 
 function App() {
+  const [theme, colorMode] = useMode()
+  // const theme2 = useTheme()
 
   return (
-    <div className="app">
-      <Header />
-      <MyResponsiveGeoMapCanvas />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content w-full h-full flex justify-center">
+            <h1>it library</h1>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
   )
 }
 
